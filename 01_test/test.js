6e5e5ce61c227d8366cca5cc8ace48f32686af9c
SifatRk <168814150+SifatRk@users.noreply.github.com>
2024-05-03 10:02:34 +0000

    math

--- 01_test/test.js
+++ 01_test/test.js
@@ -1,1 +1,10 @@
-console.log("Sifat")
+//console.log("Sifat")
+//console.log("Rahman");
+
+console.log(Math.random());
+
+const min = 10;
+const max = 20;
+
+//console.log(Math.floor(Math.random() * (max-min)) + min);
+console.log(Math.floor(Math.random() * (max - min) + min));
